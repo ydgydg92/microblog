627206aaef1322625e03319d879102003fd17593
Your Name <you@example.com>
2019-05-03 16:41:30 -0400

    mircroblog upload

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+app/__init__py:flask application instance
+
+from flask import Flask
+
+app = Flask(__name__)
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,8 @@
+app/routes.py: Home page route
+
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
