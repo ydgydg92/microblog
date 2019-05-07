e6a2d577ec4c0acce37f186448a803cfa924f15b
Your Name <you@example.com>
2019-05-07 14:38:02 -0400

    Templates

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
 #app/routes.py:Home page route
 
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='home', user=user, posts=posts)
